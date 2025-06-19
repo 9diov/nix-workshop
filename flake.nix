652ade493ac4a20822d2a0d9d6544dec495a17ba
Thanh Dinh Khac <thanhdk@gmail.com>
2025-06-19 14:01:45 +0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Holistics Nix workshop packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          # Uncomment to allow unfree packages
+          # config.allowUnfree = true;
+        };
+      in
+      {
+        packages = {
+          default = pkgs.buildEnv {
+            name = "example";
+            paths = with pkgs; [
+              hello
+              cowsay
+            ];
+          };
+          
+          # You can also define package subsets if desired
+          # Install with `nix profile intall .#minimal`
+          dev-tools = pkgs.buildEnv {
+            name = "dev-tools";
+            paths = with pkgs; [ 
+              git neovim ripgrep
+            ];
+          };
+        };
+      }
+    );
+}
